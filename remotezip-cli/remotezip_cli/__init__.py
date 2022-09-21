352fb525b853dc4a0a98004fcd7934d6c2e1ea00
Rohan Weeden <reweeden@alaska.edu>
2022-09-21 09:53:44 -0800

    Refactor and add more output to archive list mode

--- remotezip-cli/remotezip_cli/__init__.py
+++ remotezip-cli/remotezip_cli/__init__.py
@@ -2,30 +2,38 @@ import argparse
 import os
 import sys
 import urllib.parse
+from datetime import datetime
 from hashlib import sha256
+from urllib.parse import ParseResult
 
 import boto3
+import humanize
 from aws_requests_auth.aws_auth import AWSRequestsAuth
 from remotezip import RemoteZip
 
 
-def url(value: str) -> urllib.parse.ParseResult:
+def url(value: str) -> ParseResult:
     return urllib.parse.urlparse(value)
 
 
 def get_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser()
-    parser.add_argument("--profile", help="AWS profile name")
     parser.add_argument("url", help="URL of object to access", type=url)
+    parser.add_argument(
+        "filename",
+        help=(
+            "File to extract from the archive. If none provided, list the "
+            "archive contents"
+        ),
+        nargs="?"
+    )
+    parser.add_argument("--profile", help="AWS profile name")
 
     return parser
 
 
-def main():
-    parser = get_parser()
-    args = parser.parse_args()
-
-    parsed: urllib.parse.ParseResult = args.url
+def get_request_params(args: argparse.Namespace):
+    parsed: ParseResult = args.url
     if parsed.scheme == "s3":
         session = boto3.Session(profile_name=args.profile)
         credentials = session.get_credentials().get_frozen_credentials()
@@ -47,13 +55,31 @@ def main():
         auth = None
         headers = None
 
+    return url, auth, headers
+
+
+def main():
+    parser = get_parser()
+    args = parser.parse_args()
+
+    url, auth, headers = get_request_params(args)
+
     with RemoteZip(url, auth=auth, headers=headers) as rz:
-        if parsed.fragment:
-            with rz.open(parsed.fragment) as f:
+        if args.filename:
+            with rz.open(args.filename) as f:
                 with os.fdopen(sys.stdout.fileno(), "wb", closefd=False) as stdout:
                     for data in f:
                         stdout.write(data)
                     stdout.flush()
         else:
-            for f in rz.namelist():
-                print(f)
+            lines = [
+                (
+                    str(datetime(*zi.date_time)),
+                    humanize.naturalsize(zi.file_size, True),
+                    zi.filename
+                )
+                for zi in rz.infolist()
+            ]
+            max_size = max(len(file_size) for _, file_size, *_ in lines)
+            for date, file_size, *rest in lines:
+                print(date, f"{file_size:>{max_size}}", *rest)
