7e9fc47e18810827517b2c413adf5c6c5c2bf06e
Rohan Weeden <reweeden@alaska.edu>
2022-12-20 10:06:22 -0900

    Reorganize arn tests

--- destroy-cumulus/tests/test_arn.py
+++ destroy-cumulus/tests/test_arn.py
@@ -1,5 +1,9 @@
 from destroy_cumulus import Arn
 
+#
+# Colons only
+#
+
 
 def test_arn_s3_bucket():
     arn = Arn("arn:aws:s3:::bucket-name")
@@ -29,6 +33,10 @@ def test_arn_lambda_function():
     assert arn.id == "lambda-function-name"
     assert arn.type_id == "lambda:function"
 
+#
+# Only slashes in identifier
+#
+
 
 def test_arn_dynamodb_table():
     arn = Arn(
@@ -46,35 +54,6 @@ def test_arn_dynamodb_table():
     assert arn.type_id == "dynamodb:table"
 
 
-def test_arn_cloudwatch_log_group():
-    arn = Arn(
-        "arn:aws:logs:us-west-2:123456789012:"
-        "log-group:/aws/lambda/lambda-name"
-    )
-
-    assert arn.partition == "aws"
-    assert arn.service == "logs"
-    assert arn.region == "us-west-2"
-    assert arn.account == "123456789012"
-    assert arn.type == "log-group"
-    assert arn.name == "/aws/lambda/lambda-name"
-    assert arn.id == "/aws/lambda/lambda-name"
-    assert arn.type_id == "logs:log-group"
-
-
-def test_arn_api_gateway():
-    arn = Arn("arn:aws:apigateway:us-west-2::/restapis/d36my9ab58")
-
-    assert arn.partition == "aws"
-    assert arn.service == "apigateway"
-    assert arn.region == "us-west-2"
-    assert arn.account == ""
-    assert arn.type == "restapis"
-    assert arn.name == "d36my9ab58"
-    assert arn.id == "d36my9ab58"
-    assert arn.type_id == "apigateway:restapis"
-
-
 def test_arn_cloudformation():
     arn = Arn(
         "arn:aws:cloudformation:us-west-2:123456789012:"
@@ -90,3 +69,36 @@ def test_arn_cloudformation():
     assert arn.name == "dmsn-cumulus-dev-thin-egress-app"
     assert arn.id == "65d8dd50-df49-11eb-b204-020ba035f82f"
     assert arn.type_id == "cloudformation:stack"
+
+
+def test_arn_api_gateway():
+    arn = Arn("arn:aws:apigateway:us-west-2::/restapis/d36my9ab58")
+
+    assert arn.partition == "aws"
+    assert arn.service == "apigateway"
+    assert arn.region == "us-west-2"
+    assert arn.account == ""
+    assert arn.type == "restapis"
+    assert arn.name == "d36my9ab58"
+    assert arn.id == "d36my9ab58"
+    assert arn.type_id == "apigateway:restapis"
+
+#
+# Colons and slashes in identifier
+#
+
+
+def test_arn_cloudwatch_log_group():
+    arn = Arn(
+        "arn:aws:logs:us-west-2:123456789012:"
+        "log-group:/aws/lambda/lambda-name"
+    )
+
+    assert arn.partition == "aws"
+    assert arn.service == "logs"
+    assert arn.region == "us-west-2"
+    assert arn.account == "123456789012"
+    assert arn.type == "log-group"
+    assert arn.name == "/aws/lambda/lambda-name"
+    assert arn.id == "/aws/lambda/lambda-name"
+    assert arn.type_id == "logs:log-group"
