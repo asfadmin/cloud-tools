0c356f600fd2abb9df97e30403a82ea5d9abfe17
mckadesorensen <dmsorensen@alaska.edu>
2025-04-14 09:21:49 -0800

    feat: setup logging

--- test-event/test_event/main.py
+++ test-event/test_event/main.py
@@ -16,6 +16,14 @@ log = logging.getLogger(__name__)
 
 def get_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--verbose",
+        "-v",
+        help="Increase verbosity level. Can be passed multiple times.",
+        action="count",
+        default=0,
+    )
     # parser.add_argument("ingest", help="Ingest system to trigger.")
     # parser.add_argument(
     #     "--environment",
@@ -40,10 +48,27 @@ def get_parser() -> argparse.ArgumentParser:
 def main(args: Optional[list[str]] = None):
     parser = get_parser()
     pargs = parser.parse_args(args=args)
-    root_logger = logging.getLogger()
-    root_logger.addHandler(logging.StreamHandler(sys.stdout))
-    root_logger.setLevel(logging.INFO)
 
+    root_logger = logging.getLogger()
+    boto3_logger = logging.getLogger("boto3")
+    botocore_logger = logging.getLogger("botocore")
+    s3transfer_logger = logging.getLogger("s3transfer")
+
+    root_logger.setLevel(logging.INFO)
+    boto3_logger.setLevel(logging.WARNING)
+    botocore_logger.setLevel(logging.WARNING)
+    s3transfer_logger.setLevel(logging.WARNING)
+
+    if pargs.verbose >= 1:
+        root_logger.setLevel(logging.DEBUG)
+    if pargs.verbose >= 2:
+        boto3_logger.setLevel(logging.DEBUG)
+        botocore_logger.setLevel(logging.INFO)
+        s3transfer_logger.setLevel(logging.INFO)
+    if pargs.verbose >= 3:
+        botocore_logger.setLevel(logging.DEBUG)
+
+    root_logger.addHandler(logging.StreamHandler(sys.stdout))
 
     try:
         pargs.func(parser, pargs)
